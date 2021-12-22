29b1472076416ae9b567eb2750fdbd9a9b06ace5
Edgar Asatryan <nstdio@gmail.com>
2021-12-22 21:50:07 +0400

    Awful solution to sudoku problem.

--- src/main/java/io/github/nstdio/codesignal/interview/Arrays.java
+++ src/main/java/io/github/nstdio/codesignal/interview/Arrays.java
@@ -1,8 +1,6 @@
 package io.github.nstdio.codesignal.interview;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Arrays {
@@ -11,7 +9,6 @@ public class Arrays {
      * character in it. If there is no such character, return '_'.
      *
      * @param s A string that contains only lowercase English letters.
-     *
      * @return The first non-repeating character in s, or '_' if there are no characters that do not repeat.
      */
     static char firstNotRepeatingCharacter(String s) {
@@ -36,7 +33,6 @@ public class Arrays {
      * during an interview.
      *
      * @param a
-     *
      * @return
      */
     static int[][] rotateImage(int[][] a) {
@@ -58,6 +54,18 @@ public class Arrays {
         return copy;
     }
 
+    /**
+     * Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with numbers in such a way that each
+     * column, each row, and each of the nine 3 × 3 sub-grids that compose the grid all contain all of the numbers from
+     * 1 to 9 one time.
+     * <p>
+     * Implement an algorithm that will check whether the given grid of numbers represents a valid Sudoku puzzle
+     * according to the layout rules described above. Note that the puzzle represented by grid does not have to be
+     * solvable.
+     *
+     * @param grid A 9 × 9 array of characters, in which each character is either a digit from '1' to '9' or a period '.'.
+     * @return Return true if grid represents a valid Sudoku puzzle, otherwise return false.
+     */
     public static boolean sudoku2(char[][] grid) {
         final int n = grid.length;
         for (int i = 0; i < n; i++) {
@@ -65,27 +73,25 @@ public class Arrays {
             char[] ct = new char[n];
 
             for (int j = 0; j < n; j++) {
-                if (check(grid, i, rt, j))
+                if (check(grid, rt, i, j)) {
                     return false;
+                }
 
-                if (check(grid, j, ct, i))
+                if (check(grid, ct, j, i)) {
                     return false;
+                }
             }
         }
 
         return true;
     }
 
-    private static boolean check(char[][] grid, int row, char[] rt, int col) {
+    private static boolean check(char[][] grid, char[] rt, int row, int col) {
         char r = grid[row][col];
         if (r == '.')
             return false;
 
-        if (checkInBlock(grid, row, row, col)) {
-            return true;
-        }
-
-        if (checkInBlock(grid, col, row, col)) {
+        if (checkInBlock(grid, row, col)) {
             return true;
         }
 
@@ -97,20 +103,25 @@ public class Arrays {
         }
     }
 
-    private static boolean checkInBlock(char[][] grid, int cur, int row, int col) {
-        int s = (cur / 3);
-        int startIndex = Math.max(s, s * 3);
-        int endIndex = Math.min(startIndex + 3, grid.length);
-        char e = grid[row][col];
+    private static boolean checkInBlock(char[][] grid, int row, int col) {
+        // calculating 3x3 block that row belongs to
+        int sRow = row / 3;
+        int startRow = Math.max(sRow, sRow * 3);
+        int stopRow = Math.min(startRow + 3, grid.length);
 
-        for (int i = startIndex; i < endIndex; i++) {
-            for (int j = startIndex; j < endIndex; j++) {
+        // calculating 3x3 block that col belongs to
+        int sCol = col / 3;
+        int startCol = Math.max(sCol, sCol * 3);
+        int stopCol = Math.min(startCol + 3, grid.length);
+
+        for (int i = startRow; i < stopRow; i++) {
+            for (int j = startCol; j < stopCol; j++) {
                 char c = grid[i][j];
                 if (i == row && j == col || c == '.') {
                     continue;
                 }
 
-                if (c == e)
+                if (c == grid[row][col])
                     return true;
             }
         }
