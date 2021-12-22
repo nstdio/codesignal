71950a412018f74f2ff512a7460c4f3d15a09fbf
Edgar Asatryan <nstdio@gmail.com>
2021-12-23 01:57:21 +0400

    Add very basic sorting algorithms.

---
+++ src/main/java/io/github/nstdio/codesignal/Sort.java
@@ -0,0 +1,93 @@
+package io.github.nstdio.codesignal;
+
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+import static java.util.Collections.swap;
+
+/**
+ * @param <T> The element type.
+ */
+public interface Sort<T extends Comparable<? super T>> extends UnaryOperator<List<T>> {
+
+    static <E extends Comparable<? super E>> Sort<E> bubbleSort() {
+        return new BubbleSort<>();
+    }
+
+    static <E extends Comparable<? super E>> Sort<E> insertionSort() {
+        return new InsertionSort<>();
+    }
+
+    static <E extends Comparable<? super E>> Sort<E> selectionSort() {
+        return new SelectionSort<>();
+    }
+
+    class BubbleSort<E extends Comparable<? super E>> implements Sort<E> {
+        @Override
+        public List<E> apply(List<E> lst) {
+            int n = lst.size();
+            while (n > 1) {
+                int nn = 0;
+                for (int i = 1; i < n; i++) {
+                    if (lst.get(i - 1).compareTo(lst.get(i)) > 0) {
+                        swap(lst, i - 1, i);
+                        nn = i;
+                    }
+                }
+                n = nn;
+            }
+            return lst;
+        }
+
+        @Override
+        public String toString() {
+            return "BubbleSort";
+        }
+    }
+
+    class InsertionSort<E extends Comparable<? super E>> implements Sort<E> {
+        @Override
+        public List<E> apply(List<E> lst) {
+            for (int i = 1; i < lst.size(); i++) {
+                for (int j = i; j > 0 && lst.get(j - 1).compareTo(lst.get(j)) > 0; ) {
+                    swap(lst, j - 1, j);
+                    j--;
+                }
+            }
+
+            return lst;
+        }
+
+        @Override
+        public String toString() {
+            return "InsertionSort";
+        }
+    }
+
+    class SelectionSort<E extends Comparable<? super E>> implements Sort<E> {
+
+        @Override
+        public List<E> apply(List<E> lst) {
+            final int n = lst.size();
+            for (int i = 0; i < n; i++) {
+                int min = i;
+                for (int j = i; j < n; j++) {
+                    if (lst.get(j).compareTo(lst.get(min)) < 0) {
+                        min = j;
+                    }
+                }
+
+                if (min != i) {
+                    swap(lst, i, min);
+                }
+            }
+
+            return lst;
+        }
+
+        @Override
+        public String toString() {
+            return "SelectionSort";
+        }
+    }
+}
---
+++ src/test/java/io/github/nstdio/codesignal/SortTest.java
@@ -0,0 +1,46 @@
+package io.github.nstdio.codesignal;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static io.github.nstdio.codesignal.Sort.bubbleSort;
+import static io.github.nstdio.codesignal.Sort.insertionSort;
+import static io.github.nstdio.codesignal.Sort.selectionSort;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class SortTest {
+
+    static Stream<Arguments> sortingData() {
+        var lists = List.of(
+                new ArrayList<>(List.of(3, 2, 1)),
+                new ArrayList<>(List.of(1, 1, 1)),
+                new ArrayList<>(List.of(1, 2, 1, 1)),
+                new ArrayList<>(List.of(9, 5, 7, 3, 1, 8, 2, 4, 6, 3)),
+                new ArrayList<>(List.of(1)),
+                new ArrayList<>()
+        );
+
+        var sorters = List.of(selectionSort(), insertionSort(), bubbleSort());
+
+        return sorters.stream()
+                .flatMap(sorter -> lists.stream()
+                        .map(list -> arguments(sorter, list))
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sortingData")
+    void sorting(Sort<Integer> sorter, List<Integer> lst) {
+        //when
+        List<Integer> actual = sorter.apply(lst);
+
+        //then
+        assertThat(actual).isSorted();
+    }
+}
