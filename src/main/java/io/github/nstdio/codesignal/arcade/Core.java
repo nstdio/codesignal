6e4c533cee85281cf10589e9cf1102cafeb725d8
Edgar Asatryan <nstdio@gmail.com>
2021-12-26 00:41:59 +0400

    Solution for Array replace problem.

--- src/main/java/io/github/nstdio/codesignal/arcade/Core.java
+++ src/main/java/io/github/nstdio/codesignal/arcade/Core.java
@@ -666,4 +666,17 @@ public class Core {
 
         return board;
     }
+
+    /**
+     * Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+     */
+    public static int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == elemToReplace) {
+                inputArray[i] = substitutionElem;
+            }
+        }
+
+        return inputArray;
+    }
 }
