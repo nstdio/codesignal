be6d44d761a8487349b24466412ca9d44432a40d
Edgar Asatryan <nstdio@gmail.com>
2021-12-27 19:49:47 +0400

    Solution for shoppingList problem.

--- src/main/java/io/github/nstdio/codesignal/Challenge.java
+++ src/main/java/io/github/nstdio/codesignal/Challenge.java
@@ -1,5 +1,7 @@
 package io.github.nstdio.codesignal;
 
+import java.util.regex.Pattern;
+
 class Challenge {
     /**
      * Instacart customers are able to set the solution window during which they want to receive their groceries. There
@@ -49,5 +51,38 @@ class Challenge {
             ret[i] = order[1] <= fulfill && fulfill <= order[1] + order[2];
         }
         return ret;
+    }
+
+    /**
+     * As part of an Instacart beta testing group, Sara is trying out a brand new feature that automatically estimates
+     * the combined cost of the items in her handwritten shopping list. Her list contains both items and their prices.
+     * All Sara has to do is snap a photo of her list with the Instacart app, and she gets a quick estimate of what
+     * everything will cost.
+     * <p>
+     * Sara asked for your help, so it is up to you to devise an algorithm that calculates the cost after the image is
+     * converted into plain text. All you need to do is extract all numbers from the given string items and sum them
+     * up.
+     * <p>
+     * Example
+     * <p>
+     * For items = "Doughnuts, 4; doughnuts holes, 0.08; glue, 3.4", the output should be solution(items) = 7.48;
+     * <p>
+     * For items = "blue suit for 24$, cape for 12.99$ & glasses for 15.70", the output should be solution(items) =
+     * 52.69.
+     *
+     * @param items A shopping list given as a string. It is guaranteed that the only numbers in it are dollar prices
+     *              for different items. Note that although each price is given in dollars, you do not know their exact
+     *              form. Each of them can either be an integer, or a decimal number with one or two decimal places and
+     *              it may or may not be followed by a dollar sign. However, you may assume that if there is a period
+     *              ('.') between two digits, then it's a decimal mark.
+     *
+     * @return The total cost of all items.
+     */
+    static double shoppingList(String items) {
+        return Pattern.compile("\\d+(\\.\\d+)?")
+                .matcher(items)
+                .results()
+                .mapToDouble(value -> Double.parseDouble(value.group()))
+                .sum();
     }
 }
--- src/test/java/io/github/nstdio/codesignal/ChallengeTest.java
+++ src/test/java/io/github/nstdio/codesignal/ChallengeTest.java
@@ -1,6 +1,7 @@
 package io.github.nstdio.codesignal;
 
 import static io.github.nstdio.codesignal.Challenge.delivery;
+import static io.github.nstdio.codesignal.Challenge.shoppingList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
@@ -11,5 +12,14 @@ class ChallengeTest {
     void testDelivery() {
         assertThat(delivery(new int[]{200, 20, 15}, new int[][]{{300, 40, 5}, {600, 40, 10}}))
                 .isEqualTo(new boolean[]{false, true});
+    }
+
+    @Test
+    void testShoppingList() {
+        assertThat(shoppingList("blue suit for 24$, cape for 12.99$ & glasses for 15.70")).isEqualTo(52.69);
+        assertThat(shoppingList("Doughnuts, 4; doughnuts holes, 0.08; glue, 3.4")).isEqualTo(7.48);
+        assertThat(shoppingList("")).isEqualTo(0.0);
+        assertThat(shoppingList("wanna 22.2&15.3olo 0.00 and 12.12kk0.02 ..34")).isEqualTo(83.64);
+        assertThat(shoppingList("Each snack for 2.21$, each drink for 1.1")).isEqualTo(3.31);
     }
 }
