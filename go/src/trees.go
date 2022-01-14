9eeb46f768079508f0577bd49ba19808808c9279
Edgar Asatryan <nstdio@gmail.com>
2022-01-14 11:09:30 +0400

    [golang] Solution isTreeSymmetric problem

--- go/src/trees.go
+++ go/src/trees.go
@@ -1,7 +1,7 @@
 package main
 
 type Tree struct {
-	Value interface{}
+	Value int
 	Left  *Tree
 	Right *Tree
 }
@@ -10,10 +10,26 @@ func hasPathWithGivenSum(t *Tree, s int) bool {
 	if t == nil {
 		return false
 	}
-	s -= int(t.Value.(float64))
+	s -= int(t.Value)
 	if s == 0 && t.Left == nil && t.Right == nil {
 		return true
 	}
 
 	return hasPathWithGivenSum(t.Left, s) || hasPathWithGivenSum(t.Right, s)
+}
+
+func isTreeSymmetric(t *Tree) bool {
+	return isMirror(t, t)
+}
+
+func isMirror(l, r *Tree) bool {
+	if l == nil && r == nil {
+		return true
+	}
+
+	if l != nil && r != nil && l.Value == r.Value {
+		return isMirror(l.Left, r.Right) && isMirror(l.Right, r.Left)
+	}
+
+	return false
 }
--- go/src/trees_test.go
+++ go/src/trees_test.go
@@ -22,6 +22,27 @@ func TestHasPathWithGivenSum(t *testing.T) {
 	}
 }
 
+func TestIsTreeSymmetric(t *testing.T) {
+	tests := []struct {
+		expected bool
+		tree     *Tree
+	}{
+		{true, readTree("isTreeSymmetric/6.json")},
+		{true, readTree("isTreeSymmetric/5.json")},
+		{false, readTree("isTreeSymmetric/4.json")},
+		{true, &Tree{1, nil, nil}},
+		{false, &Tree{1, &Tree{1, nil, nil}, &Tree{2, nil, nil}}},
+		{false, &Tree{1, &Tree{1, nil, nil}, nil}},
+		{false, readTree("isTreeSymmetric/3.json")},
+		{false, readTree("isTreeSymmetric/2.json")},
+		{true, readTree("isTreeSymmetric/1.json")},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, isTreeSymmetric(test.tree))
+	}
+}
+
 func readTree(filePath string) *Tree {
 	open, err := os.Open("testdata/" + filePath)
 	if err != nil {
